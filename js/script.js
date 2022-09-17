b6f4e16f6bf0a03a1ce98cd59e4f2c17cd968d5b
Xerze (M. Granados) <xerzewyth@gmail.com>
2022-09-17 12:18:20 -0500

    1) Se hicieron cambios menores en el index.html. 2) En script.js se hicieron cambios importantes: Se agregaron las funciones de los botones de búsqueda y reinicio de búsqueda. Además de búsqueda y filtración en vivo mientras se teclea. Se creó una función para imprimir las tarjetas a donde se movió el código repetido del documento. Finalmente, debo aclarar que no funcionaban mis onlick ni onkeyup, así que lo resolví de otro modo dentro de js, mandando a llamar los botones con query y usando addeventlistener. 3)Finalmente en style.css simplemente se agregaron estilos a los botones de búsqueda

--- js/script.js
+++ js/script.js
@@ -16,40 +16,104 @@ class Personaje {
     }
 }
 
+let elemento = document.getElementById("div-pjs");
+let personajes = [];
+
+function buildCard(nombre, img) {
+    return `
+        <div class="card-pj">
+            <h2 class="tpj"> ${nombre} </h1>
+            <div class="each-card">
+                <img src="${img}" class="img-card">
+            </div>
+        </div>
+    `
+};
+
 // Función que nos permite obtener los personajes dela API.
 async function obtenerPersonajes() {
-    let personajes = [];
     let url = "https://akabab.github.io/starwars-api/api/all.json";
-    let elemento = document.getElementById("div-pjs");
 
+    // --------- SE RECIBEN LOS VALORES DE LA API ---------
     const response = await fetch(url);
     const data = await response.json();
 
     console.log(data);
-    console.log(personajes);
 
+    // Esta parte ejecuta un bloque de código por cada elemento de un obj iterable.
+    // En este caso, como "data" contiene un arreglo, va haciendo un for por cada posición del arreglo.
+    // En este for solamente se muestran en consola todos los personajes del arreglo.
+    /*
     for (let personaje of data) {
         console.log(personaje);
     }
+    */
 
+    // --------- SE CREAN LOS OBJETOS ---------
+    // .forEach ejecuta una función dada una vez por cada elemento del array.
+    // Es como hacer un for del tamaño del array menos uno.
     data.forEach(datum => {
+        // Aquí se está creando un nuevo personaje al que se instancian los datos.
         let nuevoPersonaje = new Personaje(datum.name, datum.image)
+        // Aquí se está agregando al arreglo de personajes, cada nuevo personaje recibido
         personajes.push(nuevoPersonaje);
     });
 
-    personajes.map((personaje) => {
+    // --------- SE IMPRIMEN LOS RESULTADOS EN PANTALLA ---------
+    // .mpa crea un nuevo arreglo con los resultados de la llamada a la función indicada,
+        // aplicados a cada uno de sus elementos.
+    personajes.forEach((personaje) => {
         // Con esto inyectamos al HTML el código para que muestre el resultado.
-        elemento.innerHTML += `
-        <div class="card-pj">
-            <h1 class="tpj"> ${personaje.obtenerNombre()}</h1>
-            <p>
-                <img src=${personaje.obtenerImg()} height=500px>
-            </p>
-        </div>
-        
-    `
+        elemento.innerHTML += buildCard(
+            personaje.obtenerNombre(),
+            personaje.obtenerImg()
+            )
     })
-}
 
+    /*
+    let busquedaBtn = document.getElementById("busqueda-btn");
+    let reiniciarBtn = document.getElementById("reinicio-btn");
+    busquedaBtn.onclick = llamarBusqueda(onclick);
+    reiniciarBtn.onclick = reiniciarBusqueda(onclick);
+    */
+
+    let busquedaBtn = document.querySelector('#busqueda-btn');
+    let reiniciarBtn = document.querySelector("#reiniciar-btn");
+    let tecleoBuscar = document.querySelector("#buscador");
+
+    busquedaBtn.addEventListener('click', llamarBusqueda, false);
+    reiniciarBtn.addEventListener('click', reiniciarBusqueda, false);
+    tecleoBuscar.addEventListener('keyup', llamarBusqueda, false);
+
+    function reiniciarBusqueda() {
+        personajes.length = 0;
+        elemento.innerHTML = null;
+        obtenerPersonajes();
+    }
+    
+    function llamarBusqueda() {
+        // El set time out es para que ejecute la función un momento después de que se presione la tecla.
+        // En buscadores, se procura usar un setTimeout de medio segundo.
+        setTimeout(() => {
+            const consulta = document.getElementById("buscador").value;
+            const busqueda = personajes.filter(
+                personaje => personaje.name.toLowerCase().includes(consulta.toLowerCase())
+                );
+    
+            if (busqueda.length > 0) {
+                // Se borran todas las tarjetas.
+                elemento.innerHTML = null;
+
+                busqueda.forEach((busquedaPj) => {
+                    elemento.innerHTML += buildCard(
+                        busquedaPj.obtenerNombre(),
+                        busquedaPj.obtenerImg())
+                })
+            }
+        }, 500)
+        
+    }
+
+}
 
 obtenerPersonajes();
